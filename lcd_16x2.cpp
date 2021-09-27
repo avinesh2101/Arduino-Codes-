17221f9886c5b44025f22aa3c140943995798033
Avinesh <avineshpandey2101@gmail.com>
2021-09-27 16:22:56 +0530

    first commit

---
+++ lcd_16x2.cpp
@@ -0,0 +1,31 @@
+#include <LiquidCrystal.h>
+
+
+//        (resisotor pin,enable pin,DB4--DB7)
+//        LiquidCrystal lcd(12,11,5,4,3,2);
+//
+//        void setup()
+//        {
+//          lcd.begin(16,2);
+//            lcd.setCursor(15,0);
+//              lcd.print("yoyoyoy");
+//              }
+//
+//              void loop() {
+//               
+//
+//                 for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+//                     // scroll one position left:
+//                         lcd.scrollDisplayLeft();
+//                             // wait a bit:
+//                                 delay(150);
+//                                   }
+//
+//                                     
+//
+//                                      
+//
+//                                      }
+//
+//
+//
