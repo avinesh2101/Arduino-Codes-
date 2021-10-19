2cebeff880c1cb875e14f8e6354d1e1db1caf907
Avinesh <avineshpandey2101@gmail.com>
2021-10-19 22:47:00 +0530

    added tempelate

---
+++ tempelate.cpp
@@ -0,0 +1,9 @@
+#include< >
+//add library and with brief explanation of library or add prorper links in comment
+
+void setup()
+{
+    //try to write comment explaining the working of the logic 
+}
+
+//that's it just make sure to write readable code which is easy to understand with proper comments
