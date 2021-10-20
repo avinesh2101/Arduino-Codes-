377857d4651b831c11741fa0a0621eba649c760d
Uttam Kumar Sharma <64878251+uttams237@users.noreply.github.com>
2021-10-20 23:25:08 +0530

    Create code.cpp

---
+++ knightRider/code.cpp
@@ -0,0 +1,79 @@
+int pin2 = 2;
+int pin3 = 3;
+int pin4 = 4;
+int pin5 = 5;
+int pin6 = 6;
+int pin7 = 7;
+
+int delayTime = 100;
+
+void setup(){
+pinMode(pin2, OUTPUT);
+pinMode(pin3, OUTPUT);
+pinMode(pin4, OUTPUT);
+pinMode(pin5, OUTPUT);
+pinMode(pin6, OUTPUT);
+pinMode(pin7, OUTPUT);
+}
+void loop() {
+digitalWrite(pin2, HIGH);
+delay(delayTime);
+
+digitalWrite(pin2, LOW);
+delay(delayTime);
+
+digitalWrite(pin3, HIGH);
+delay(delayTime);
+
+digitalWrite(pin3, LOW);
+delay(delayTime);
+
+digitalWrite(pin4, HIGH);
+delay(delayTime);
+
+digitalWrite(pin4, LOW);
+delay(delayTime);
+
+digitalWrite(pin5, HIGH);
+delay(delayTime);
+
+digitalWrite(pin5, LOW);
+delay(delayTime);
+
+digitalWrite(pin6, HIGH);
+delay(delayTime);
+
+digitalWrite(pin6, LOW);
+delay(delayTime);
+
+digitalWrite(pin7, HIGH);
+delay(delayTime);
+
+digitalWrite(pin7, LOW);
+delay(delayTime);
+
+digitalWrite(pin6, HIGH);
+delay(delayTime);
+
+digitalWrite(pin6, LOW);
+delay(delayTime);
+
+digitalWrite(pin5, HIGH);
+delay(delayTime);
+
+digitalWrite(pin5, LOW);
+delay(delayTime);
+
+digitalWrite(pin4, HIGH);
+delay(delayTime);
+
+digitalWrite(pin4, LOW);
+delay(delayTime);
+
+digitalWrite(pin3, HIGH);
+delay(delayTime);
+
+digitalWrite(pin3, LOW);
+delay(delayTime);
+
+}
